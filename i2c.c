1fa3c4368461dc3fd3d194fd0325002ea96df2b0
Tyler Nielsen <tylernielsenn@gmail.com>
2024-08-20 22:34:23 -0600

    progress on i2c:

--- i2c.c
+++ i2c.c
@@ -10,11 +10,25 @@
 #define MT_DATA_ACK     0x28
 #define I2C_WRITE_BIT   0x00
 #define I2C_READ_BIT    0x01
+#define READ_SLA_ACK    0x40
 
 
 int8_t i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t length, void *intf_ptr){
 
-    
+    TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWSTO)|(1<<TWEN); 
+
+    while(!(TWCR & (1<<TWINT))); 
+
+    if((TWSR & 0xF8) != START) return 1;
+
+    TWDR = (reg_addr << 1) | I2C_READ_BIT; 
+    TWCR = (1<<TWINT)|(1<<TWEN); 
+
+    while(!(TWCR & (1<<TWINT))); 
+
+    if(!(TWSR & 0xF8) != READ_MT_SLA_ACK) return 1; 
+
+
 
     return 0; 
 }
